259a6f953ccbeacbfac656ccd0ac3885c0111167
colinxs <me@colinxsummers.com>
2020-03-25 22:21:14 -0700

    hello world

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,36 @@
 using SpecialArrays
 using Test
 
+using Base: index_shape, index_dimsum, index_ndims, to_indices
+using Test
+using Random
+
+using AxisArrays: AxisArrays
+using UnsafeArrays
+using BenchmarkTools
+using StaticNumbers
+
+using LyceumBase.LyceumCore
+using SpecialArrays: _maybe_unsqueeze
+using LyceumBase.TestUtil
+
+
+include("testutil.jl")
+
+nones(N::Integer) = ntuple(_ -> 1, Val(unstatic(N)))
+
+testdims(L::Integer) = ntuple(i -> 3 + i, Val(unstatic(L)))
+
+#@testset "functions" begin
+#    include("functions.jl")
+#end
+
+#@testset "elasticarray" begin
+#    include("elasticarray.jl")
+#end
+
 @testset "SpecialArrays.jl" begin
-    # Write your own tests here.
+    @testset "slices" begin
+        include("slices.jl")
+    end
 end
