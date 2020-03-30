4c74112783bfc44866c63d014dd58e9469e8e876
colinxs <me@colinxsummers.com>
2020-03-29 21:40:13 -0700

    use @includetests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,6 @@ module TestSpecialArrays
 
 include("preamble.jl")
 
-if isempty(ARGS)
-    TEST_FILES =
-        sort([file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing])
-else
-    TEST_FILES = ARGS
-end
-
-@testset "$file" for file in TEST_FILES
-    include(file)
-end
+@includetests ProgressTestSet "SpecialArrays"
 
 end # module
